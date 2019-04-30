020194823a041749f82d8f822bce15e8f04c4208
ctfang <1992053348@qq.com>
2019-04-30 13:44:42 +0800

    base

--- examples/baseserver/main.go
+++ examples/baseserver/main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"github.com/ctfang/network"
 	"github.com/ctfang/network/tcp"
 )
 
 func main() {
-	fmt.Println(tcp.Server{})
+	server := tcp.Server{}
+	server.SetAddress(network.NewAddress("127.0.0.1:8080"))
+	server.SetConnectionEvent(&baseevent{})
+	server.ListenAndServe()
+}
+
+type baseevent struct {
+}
+
+func (*baseevent) OnStart(listen network.ListenTcp) {
+	println("implement me")
+}
+
+func (*baseevent) OnConnect(connect network.Connect) {
+	println("implement me")
+}
+
+func (*baseevent) OnMessage(connect network.Connect, message interface{}) {
+	println("implement me")
+}
+
+func (*baseevent) OnClose(connect network.Connect) {
+	println("implement me")
+}
+
+func (*baseevent) OnError(listen network.ListenTcp, err error) {
+	println("implement me")
 }
